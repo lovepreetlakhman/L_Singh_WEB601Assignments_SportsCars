c940bd2224be0dad10063110a83a60f02dcf9c2a
Lovepreet Singh <105748512+lovepreetlakhman@users.noreply.github.com>
2023-02-17 21:24:56 -0500

    type done

--- L_Singh_SportsCars/src/app/content-type.pipe.ts
+++ L_Singh_SportsCars/src/app/content-type.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Content } from './helper-files/content-interface';
 
 @Pipe({
   name: 'contentType'
 })
 export class ContentTypePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(cards: Content[], type: string): Content[] {
+    if(!type){
+      return cards.filter(card => !card.type)
+    }
+
+    return cards.filter(card => card.title === type);
   }
 
 }
